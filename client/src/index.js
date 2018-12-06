5336be94dd1a23aa327117f537b58361569707d3
Sean Percy <s58562@beuth-hochschule.de>
2018-12-06 19:26:23 +0100

    [#17] set up react stub

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,9 @@
-console.log('Testing React Webpack Babel Setup');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Testing React Webpack Babel Setup';
+
+ReactDOM.render(
+	<div>{title}</div>,
+	document.getElementById('app')
+);
